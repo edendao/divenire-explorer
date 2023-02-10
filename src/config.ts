930363f69f9fa6d0e6f2d9267260e245c8a91bc1
Raffaele <izziaraffaele@gmail.com>
2023-02-10 09:18:18 +0100

    fix: build + gitpod config

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,10 @@ export const APP = {
   keywords: [],
 };
 
+export const ICON = {
+  NAVBAR_ITEM: 22,
+  NAVBAR_ITEM_HORIZONTAL: 20,
+};
 // LAYOUT
 // ----------------------------------------------------------------------
 
